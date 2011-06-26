e5b84db12a8a8d1377db7ddf1474d6e03af665e0
Modest Syla <modest.syla@gmail.com>
2011-06-25 21:03:57 -0400

    Still learning mercurial.  Adding the rest of the files for annotation support.

---
+++ root/object-criteria-factory/src/test/java/net/freedom/gj/beans/factory/AnnotationDefaultTreeBeanFactoryTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2009-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.freedom.gj.beans.factory;
+
+import net.freedom.gj.beans.criteria.MapContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author martin
+ */
+public class AnnotationDefaultTreeBeanFactoryTest {
+    
+    BeanFactory<MyService,Object> factory;
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of registerBean method, of class DefaultTreeBeanFactory.
+     */
+    @Before
+    public void registerBeans() throws Exception{
+       
+        // create implementations
+        MyAnnotatedServiceA d = new MyAnnotatedServiceA();
+        MyAnnotatedServiceB e = new MyAnnotatedServiceB();
+        MyAnnotatedServiceC c = new MyAnnotatedServiceC();
+        
+        
+        System.out.println("registerBean");
+        DefaultTreeBeanFactory<MyService,Object> instance = new DefaultTreeBeanFactory<MyService,Object>();
+        instance.setObjectType("net.freedom.gj.beans.factory.MyService");
+        instance.register(d,e,c);
+        
+        instance.init();
+        
+        factory = instance;
+    }
+    
+    @Test
+    public void testFactoryUsingMapContext() throws Exception{
+        
+         MyService service = factory.getObject(new MapContext("payload","I am a string value") );
+         
+         service.execute();
+         
+         service = factory.getObject(new MapContext("payload",new Integer(0),"switch","ON" ) );
+        
+         service.execute();
+         
+         DataEntity pojo = new DataEntity("ABC","furniture");
+         
+         service = factory.getObject(pojo);
+         
+         service.execute();
+         
+    }
+    
+   
+       
+    
+}
